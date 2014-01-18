5cf49d90fd51f089b74977686afdfcd23a401b60
Larry Hipp <larry.hipp@gmail.com>
2014-01-18 10:23:48 -0500

    stage

---
+++ public/js/controllers/routes.js
@@ -0,0 +1,39 @@
+'use strict';
+
+angular.module('mean.routes').controller('RoutesController', ['$scope', '$routeParams', '$location', 'Global', 'Routes', function ($scope, $routeParams, $location, Global, Routes) {
+    $scope.global = Global;
+
+    $scope.create = function() {
+        var model = new Routes({
+            action: 	this.action,
+            controller: this.controller,
+            path: 		this.path,
+            verb:       this.verb
+        });
+
+        console.log(model);
+
+        model.$save(function(response) {
+            $location.path('routes');
+        });
+
+        this.action     = '';
+        this.controller = '';
+        this.path       = '';
+        this.verb       = '';
+    };
+
+    $scope.find = function() {
+        Routes.query(function(routes) {
+            $scope.routes = routes;
+            console.log(routes);
+        });
+    };
+
+    $scope.findOne = function() {
+        Routes.get({routeId: $routeParams.routeId}, function(route) {
+            $scope.route = route;
+        });
+    };
+
+}]);
