ee11e3461ed23d63eed40d0cd299ab0dcf5ff62a
Larry Hipp <larry.hipp@gmail.com>
2014-01-19 10:29:30 -0500

    removing comments

--- app/controllers/articles.js
+++ app/controllers/articles.js
@@ -1,16 +1,9 @@
 'use strict';
 
-/**
- * Module dependencies.
- */
 var mongoose    = require('mongoose'),
     Article     = mongoose.model('Article'),
     _           = require('lodash');
 
-
-/**
- * Find article by id
- */
 exports.article = function(req, res, next, id) {
     Article.load(id, function(err, article) {
         if (err) return next(err);
@@ -21,9 +14,6 @@ exports.article = function(req, res, next, id) {
     });
 };
 
-/**
- * Create a article
- */
 exports.create = function(req, res) {
     var article = new Article(req.body);
     article.user = req.user;
@@ -40,9 +30,6 @@ exports.create = function(req, res) {
     });
 };
 
-/**
- * Update a article
- */
 exports.update = function(req, res) {
     var article = req.article;
 
@@ -60,9 +47,6 @@ exports.update = function(req, res) {
     });
 };
 
-/**
- * Delete an article
- */
 exports.destroy = function(req, res) {
     var article = req.article;
 
@@ -78,16 +62,10 @@ exports.destroy = function(req, res) {
     });
 };
 
-/**
- * Show an article
- */
 exports.show = function(req, res) {
     res.jsonp(req.article);
 };
 
-/**
- * List of Articles
- */
 exports.all = function(req, res) {
     Article.find().sort('-created').populate('user', 'name username').exec(function(err, articles) {
         if (err) {
